b4780af1c73682134d8059cfd24ac6603cd2a7de
José Briseño <josecarlos.briseno00@gmail.com>
2023-04-14 18:04:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import Weather from './components/Weather'
+import Loader from './components/Loader'
+
+function App() {
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temps, setTemps] = useState()
+
+  const success = (position) => { 
+    const currentCoords = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    }
+    setCoords(currentCoords)
+   }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success)  
+  }, [])
+  
+
+
+  useEffect(() => {
+    if (coords) {
+     const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=ee552535f4811bc725b6e1493386cb42`
+      console.log(endpoint)
+    axios
+      .get(endpoint)
+      .then((res) => {
+        setWeather(res.data)
+        const celsius = (res.data.main.temp -273.15).toFixed(1)
+        const fahrenheit = (celsius * 9 /5 + 32).toFixed(1)
+        const temps = {
+          celsius,
+          fahrenheit
+        }
+        setTemps(temps)
+      })
+      .catch((err) => { console.log(err) })
+    }
+  }, [coords])
+
+    return (
+    <div className={`App flex justify-center items-center min-h-screen bg-[url('/images/bg/${weather?.weather[0].icon}.jpg')]  bg-cover bg-center`}>
+      {
+        weather ? 
+        (<Weather weather={weather} temps={temps}/>) :
+        (<Loader/> )
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div className=' flex justify-center items-center min-h-screen text-lg'>
+      <div className='loader-09 bg-black h-16 w-16'></div>
+    </div>
+  )
+}
+
+export default Loader
